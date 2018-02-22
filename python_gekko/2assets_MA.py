010bb86dd4ef7bd3d9068ac04e9b9c2c99af4a13
fredmanre <fredmanre@gmail.com>
2018-02-22 11:43:23 -0400

    estrategia para MA con btc

--- python_gekko/2assets_MA.py
+++ python_gekko/2assets_MA.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 # Global variables.
 prices = pd.DataFrame(columns=['open', 'high', 'low', 'close',
-                               'vwp', 'volume', 'trades'])
+                               'vwp', 'volume', 'trades', 'datetime'])
+
 bars = set()
 in_market = False
 
@@ -17,7 +18,7 @@ def reset_settings():
     global prices, bars, in_market
 
     prices = pd.DataFrame(columns=['open', 'high', 'low', 'close',
-                                   'vwp', 'volume', 'trades'])
+                                   'vwp', 'volume', 'trades', 'datetime'])
     bars = set()
     in_market = False
 
@@ -31,7 +32,6 @@ def strategy():
     # print(body)
     counter = int(body['counter'])
     settings = body['settings']
-    print(settings)
     candle = body['candle']
     advice = {'long': False,
               'short': False}
@@ -42,7 +42,7 @@ def strategy():
 
     # Storing new price.
     time = parser.parse(candle['start'])
-    id = candle['id']
+    # id = candle['id']
     new_price = pd.DataFrame({'open': candle['open'], 
                               'high': candle['high'],
                               'low': candle['low'],
@@ -50,15 +50,57 @@ def strategy():
                               'vwp': candle['vwp'],
                               'volume': candle['volume'],
                               'trades': candle['trades'],
+                              'datetime': candle['start']
                               }, 
-                             index = [id])
+                             index = [time])
     prices = prices.append(new_price)
 
     # Strategy logic.
-    MovinAverage = prices['close'].rolling(10).mean()
-    MovinAverage20 = prices['close'].rolling(20).mean()
-    # print(MovinAverage)
-    # print(MovinAverage20)
+    if settings['type'] == 'btc':
+        if settings['override']  == 'yes':
+            # calculo para el MAdif
+            malong = settings['longperiod']
+            MAlong = prices['close'].rolling(malong).mean()
+            indice = len(MAlong) - 1
+            mashort = settings['shortperiod']
+            MAshort = prices['close'].rolling(mashort).mean()
+            indice1 = len(MAshort) - 1
+            # MAdif
+            MAdif = (1 - (MAshort[indice1]/MAlong[indice]))*100
+            # ultima fecha
+            fecha = prices.iloc[-1]['datetime']
+            time1 = parser.parse(fecha)  # fecha parseada
+
+            if float(MAdif) > settings['percentage']:
+                a = 'long'
+            else:
+                a = 'short'
+            # para la estrategia
+            nestrategia = {'fecha': fecha, 'tend': a, 'MAdif': MAdif, 'date': time1}
+            # se debe guardar en un archivo .scv indice, fecha, MAdif y a
+            estrategia = pd.DataFrame({'fecha': [fecha],
+                                        'tend':[a],
+                                        'MAdif':[MAdif]
+                                        },columns=['fecha', 'tend', 'MAdif'],
+                                        index=[time1])
+            estrategia.to_csv('./static/MAdif.csv')
+            # print('Malong: \n', MAlong)
+            print('último MAlong: ', MAlong[indice])
+            # print('MAshort: \n', MAshort)
+            print('último MAshort: ', MAshort[indice])
+            print('MAdif: ', MAdif)
+            print(a)
+            print(time1)
+            print(fecha)
+            print(nestrategia)
+            print(estrategia)
+        else:  # if override != 'yes'
+            pass
+    else:
+        estrategia_scv = pd.read_csv('./static/MAdif.csv')
+        print(estrategia_scv)
+        print(estrategia_scv.columns)
+
 
     # Bullish signal.
 
@@ -67,7 +109,7 @@ def strategy():
     body['trend'] = ''
     body['advice'] = ''
     # print(prices)
-    # print('prueba')
+    # print(len(prices))
  
     return jsonify(body)
 
